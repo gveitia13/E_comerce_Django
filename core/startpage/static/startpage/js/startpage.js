6775ca4bf428238d14ba67163cd49ec64f78d1bc
Gilbert Veitia <gveitia13@gmail.com>
2021-10-04 11:05:11 +0200

    arreglar productos cuando actualiza ventas con stock en 0 arreglar subida de fotos en update en productos editar form producto login: validators terminar el startpage backend Casi terminado el startpage Hacer el panel de admin

--- core/startpage/static/startpage/js/startpage.js
+++ core/startpage/static/startpage/js/startpage.js
@@ -25,9 +25,13 @@ $(function () {
       })
     }, 250)
   })
+
   //Obteniendo detalles del producto por ajax
-  d.querySelectorAll('.div-img').forEach(e =>
-    e.addEventListener('click', function () {
+  d.querySelectorAll('div.prod-card').forEach(e =>
+    e.addEventListener('click', function (f) {
+
+      if (f.target.closest('button.prod-id')) return
+
       let parameters = new FormData()
       parameters.append('action', 'getProd')
       parameters.append('id', this.id)
@@ -49,12 +53,14 @@ $(function () {
   d.querySelector('a[rel="contact"]').addEventListener('click', () =>
     $('#contact').modal('show')
   )
+
   //Eliminar de la tabla del carrito y evento cantidad
   $('#prods-cart tbody')
     .on('click', 'a[rel="delete"]', function () {
       let tr = listTableCart.cell($(this).closest('td, li')).index()
       Cart.items.prods.splice(tr.row, 1)
       Cart.list()
+      if (!Cart.items.prods.length) resetForm()
     })
     .on('change', 'input[name="cantidad"]', function () {
       let tr = listTableCart.cell($(this).closest('td, li')).index()
@@ -64,12 +70,14 @@ $(function () {
         `$${Cart.items.prods[tr.row].subtotal.toFixed(2)}`
       )
     })
+
   //adicionando al carro por los botones
   d.querySelectorAll('button.prod-id').forEach(e =>
     e.addEventListener('click', function () {
       let parameters = new FormData()
       parameters.append('action', 'getProd')
       parameters.append('id', this.name)
+
       ajaxFunction(location.pathname, parameters, data => {
         // console.log(data)
         if (!Cart.items.prods.find(e => e.id === data.id)) {
@@ -89,22 +97,57 @@ $(function () {
   d.querySelector('button.local')
     .addEventListener('click', () => btnLocal())
   window.addEventListener('load', () => {
-    console.log('hola')
     if (d.querySelector('button.home').classList.contains('active'))
       btnHome()
     if (d.querySelector('button.local').classList.contains('active'))
       btnLocal()
   })
+
+  $('#cart').on('hidden.bs.modal', () => {
+    if (!Cart.items.prods.length) resetForm()
+  })
+
+  //Eventos inputs del cart
+  writeInValue('.form-cart-name', '.form-cart-name-1')
+  writeInValue('.form-cart-addr', '.form-cart-addr-1')
+  writeInValue('.form-cart-note', '.form-cart-note-1')
+  errorEmptyValue('.form-cart-name')
+  errorEmptyValue('.form-cart-name-1')
+  errorEmptyValue('.form-cart-addr-1')
+  errorEmptyValue('.form-cart-addr')
+
+  $('#form-cart').on('submit', function (e) {
+    e.preventDefault()
+    if (!Cart.items.prods.length)
+      return Alerta('The shopping cart is empty', 'error')
+
+    //Si no se ha echo focus y se da submit pone clase a los vacios
+    validateEmptySubmit('.form-cart-name')
+    validateEmptySubmit('.form-cart-name-1')
+    validateEmptySubmit('.form-cart-addr-1')
+    validateEmptySubmit('.form-cart-addr')
+
+    //Ultimas validaciones por resolution
+    if (d.querySelector('button.home').classList.contains('active')) {
+      if ($(window).width() < 575)
+        lastSubmitValidate2('.form-cart-name-1', '.form-cart-addr-1')
+      else
+        lastSubmitValidate2('.form-cart-name', '.form-cart-addr')
+    } else {
+      if ($(window).width() < 575)
+        lastSubmitValidate('.form-cart-name-1')
+      else
+        lastSubmitValidate('.form-cart-name')
+    }
+  })
 })
 const d = document
-let removeAcents = str => {
+let
+  removeAcents = str => {
     const acents = {á: 'a', é: 'e', í: 'i', ó: 'o', ú: 'u', ñ: 'n'}
-    return str
-      .split('')
-      .map(e => acents[e] || e)
-      .join('')
-      .toString()
-  },
+    return str.split('').map(e => acents[e] || e).join('').toString()
+  }
+  ,
   Alerta = (text, icon = 'success') => {
     const Alerta = Swal.mixin({
       toast: true,
@@ -116,7 +159,8 @@ let removeAcents = str => {
       icon: icon,
       title: text
     })
-  },
+  }
+  ,
   btnHome = () => {
     d.querySelector('button.home').classList.add('active')
     d.querySelector('i.home').classList.add('mdi-check')
@@ -128,7 +172,8 @@ let removeAcents = str => {
     )
     d.querySelector('p.horario').classList.remove('d-none')
     d.querySelector('p.direccion').classList.add('d-none')
-  },
+  }
+  ,
   btnLocal = () => {
     d.querySelector('button.home')
       .classList.remove('active')
@@ -141,11 +186,110 @@ let removeAcents = str => {
     )
     d.querySelector('p.horario').classList.add('d-none')
     d.querySelector('p.direccion').classList.remove('d-none')
-  },
+  }
+  ,
+  writeInValue = (selector, selector1) => {
+    d.querySelector(`${selector}`)
+      .addEventListener('input', function () {
+        d.querySelector(`${selector1}`).value = this.value
+      })
+    d.querySelector(`${selector1}`)
+      .addEventListener('input', function () {
+        d.querySelector(`${selector}`).value = this.value
+      })
+  }
+  ,
+  errorEmptyValue = selector => {
+    d.querySelector(`${selector}`).addEventListener('blur', function () {
+      if (!Cart.items.prods.length) return
+      if (!(this.value)) this.classList.add('is-invalid')
+      else this.classList.remove('is-invalid')
+    })
+    d.querySelector(`${selector}`).addEventListener('focus', function () {
+      if (!Cart.items.prods.length) return
+      if (!(this.value)) this.classList.remove('is-invalid')
+      // else this.classList.add('is-invalid')
+    })
+  }
+  ,
+  checkIsValidClass = selector => d.querySelector(`${selector}`).classList.contains('is-invalid')
+  ,
+  validateEmptySubmit = selector => {
+    if (d.querySelector(`${selector}`).value)
+      return true
+    d.querySelector(`${selector}`).classList.add('is-invalid')
+    return false
+  }
+  ,
+  resetForm = () => {
+    d.querySelectorAll('[class*="form-cart-name"]').forEach(e => {
+      e.classList.remove('is-invalid')
+      e.value = ''
+    })
+    d.querySelectorAll('[class*="form-cart-addr"]').forEach(e => {
+      e.classList.remove('is-invalid')
+      e.value = ''
+    })
+    d.querySelectorAll('[class*="form-cart-note"]').forEach(e => {
+      e.value = ''
+    })
+  }
+  ,
+  lastSubmitValidate2 = (selector, selector1) => {
+    if (checkIsValidClass(`${selector}`) ||
+      checkIsValidClass(`${selector1}`)) {
+      Alerta('Complete the form, please', 'error')
+    } else {
+      window.open(`https://api.whatsapp.com/send/?phone=+5358496023&text=${generateMSG()}&app_absent=1`)
+      console.log(generateMSG())
+      $('#cart').modal('hide')
+      //link de whatsapp
+      Cart.items.prods = []
+      Cart.list()
+      resetForm()
+    }
+  }
+  ,
+  lastSubmitValidate = selector => {
+    if (checkIsValidClass(`${selector}`)) {
+      Alerta('Complete the form, please', 'error')
+    } else {
+      window.open(`https://api.whatsapp.com/send/?phone=+5358496023&text=${generateMSG()}&app_absent=1`)
+      console.log(generateMSG())
+      $('#cart').modal('hide')
+      //link de whatsapp
+      Cart.items.prods = []
+      Cart.list()
+      resetForm()
+    }
+  }
+  ,
+  generateMSG = () => {
+    let table = ''
+    Cart.items.prods.forEach(e => table += `${e.name} (${e.s_price}) * ${e.cant}%0A`)
+    table += `%0ATotal: ${d.querySelector('span.cart-total').innerText}%0A%0A`
+
+    let dataClient = 'Name: ' + d.querySelector('.form-cart-name').value + '%0A'
+    if (d.querySelector('.form-cart-note').value)
+      dataClient += `Note: ${d.querySelector('.form-cart-note').value}%0A`
+
+    if (d.querySelector('button.home').classList.contains('active')) {
+      let str = 'Hola, me gustaría comprar los siguientes productos y que me los traigan a la dirección: %0A'
+        + d.querySelector('.form-cart-addr').value + '%0A%0A'
+      str += table
+      str += dataClient
+      return str
+    } else {
+      let str = 'Hola me gustaría comprar los siguientes productos: %0A%0A'
+      str += table
+      str += dataClient
+      return str
+    }
+  }
+  ,
   Cart = {
     items: {
       prods: [],
-      // subtotal: 0.00,
       total: 0.0
     },
     get_ids: () => Cart.items.prods.map(value => value.id),
@@ -209,7 +353,7 @@ let removeAcents = str => {
             render: (data, type, row) =>
               $(window).width() <= 576
                 ? truncate(data, 9, '...') + `<br> <div class="text-xs">$${row.s_price}</div>`
-                : truncate(data, 17, '...') + `\n ${row.s_price}`
+                : truncate(data, 17, '...') + `<br> <div class="text-xs">$${row.s_price}</div>`
           }
         ],
         rowCallback(row, data, displayNum, displayIndex, dataIndex) {
